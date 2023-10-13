f0c2a0d22876fe19fe9aaebdc99e9c21ae5dae51
Heba Derawi <heba.derawi95@hotmail.com>
2023-10-13 11:38:01 +0200

    ändring

--- Program.cs
+++ Program.cs
@@ -16,85 +16,85 @@ namespace Labb6___OOP_Arv
 
     public class Animal
     {
-       public string Color { get; set; }
-       public string Name {  get; set; }  
+       public string Breed { get; set; }
+       public string Color { get; set; } 
        public  int Age {  get; set; }   
 
      
 
         public void Jump ()
         {
-            Console.WriteLine(Name + " is jumping "); 
+            Console.WriteLine(Breed + " is jumping "); 
         }
         public void Run () 
         {
-            Console.WriteLine(Name + " is running");
+            Console.WriteLine(Breed + " is running");
         }
-        public void Bark () 
+        public void play () 
         {
-            Console.WriteLine(Name + "is playing");
+            Console.WriteLine(Breed + "is playing");
         }
     }
     public class Dog : Animal
     {
         string Size;
-        public Dog(string color_ =null, string name_ =null, int Age_ =0, string size_=null)
+        public Dog(string breed_ =null, string color_ = null, int Age_ =0, string size_=null)
         {
+            breed_ = Breed;
             color_ = Color;
-            name_ = Name;
             Age_ = Age;
             size_ = Size;
         }
 
         public void Eat()
         {
-            Console.WriteLine(Name + "is eating");
+            Console.WriteLine(Breed + "is eating");
         }
        public void makeSound()
         {
-            Console.WriteLine( "The" + Name +  "is barking");
+            Console.WriteLine( "The" + Breed +  "is barking");
         }
     }
 
     public class Cat : Animal
     {
         String Eyecolor;
-        public Cat(string color_ = null, string name_ = null, int Age_ =0, string eyecolor_ =null)
+        public Cat(string breed_, string color_ = null, int Age_ =0, string eyecolor_ =null)
         {
             color_ = Color;
-            name_ = Name;
+            breed_ = Breed;
             Age_ = Age;
             eyecolor_ = Eyecolor;
         }
 
         public void Cuddle()
         {
-            Console.WriteLine("The " + Name + "is cuddling");
+            Console.WriteLine("The " + Breed + "is cuddling");
         }
 
         public void makeSound()
         {
-            Console.WriteLine("The" + Name + "meowing");
+            Console.WriteLine("The" + Breed + "meowing");
         }
     }
     public class Bird : Animal
     {
         String Feathers;
-        public Bird(string color_ = null, string name_ = null, int Age_ = 0, string feathers = null)
+        public Bird(string breed_, string color_ = null, int Age_ = 0, string feathers = null)
         {
             color_ = Color;
-            name_ = Name;
+            breed_ = Breed;
             Age_ = Age;
             Feathers = feathers;
         }
         public void hunt() 
         {
-            Console.WriteLine("The" + Name + " is hunting");
+            Console.WriteLine("The" + Breed + " is hunting");
         }
 
         public void makeSound()
         {
-            Console.WriteLine("The" + Name + " is whistling");
+            Console.WriteLine("The" + Breed + " is whistling");
         }
     }
 }
