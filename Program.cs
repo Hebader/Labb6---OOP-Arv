2a1f0be277c246bf9446e93cd563ba5190624b40
Heba Derawi <heba.derawi95@hotmail.com>
2023-10-13 10:28:42 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Labb6___OOP_Arv
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+
+    public class Animal
+}
